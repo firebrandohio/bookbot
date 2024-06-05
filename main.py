57c98fb0933f0455bd1fb60ba084ac8bea65bcfc
firebrandohio <firebrandohio@protonmail.com>
2024-06-05 18:39:01 -0400

    completed basic functionality

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-def main():
+#!/usr/bin/env python3
+def main(filepath):
     file_contents = None
-    with open("books/frankenstien.txt") as f:
+    with open(filepath) as f:
         file_contents = f.read()
 
     words = count_words(file_contents)
     characters = sort_characters(count_characters(file_contents))
     
-    
-    print(characters)
+    print_report(filepath, words, characters)
 
 def count_words(str):
     words = str.split()
@@ -33,10 +33,22 @@ def sort_characters(char_count):
     
     char_count_list.sort(reverse=True, key=sort_on)
     return char_count_list
-    
-
 
 def sort_on(dict):
     return dict["count"]
 
-main()
+def print_report(filepath, words, char_count_list):
+    print(f"--- Begin Report of {filepath} ---")
+    print("Wordcount")
+    print(f"  * Document contains {words} words")
+    print("Character Frequency")
+    for struct in char_count_list:
+        char = struct["char"]
+        count = struct["count"]
+        if count > 1:
+            print(f"  * '{char}' appears {count} times")
+        else:
+            print(f"  * '{char}' appears {count} time")
+    print("--- End Report ---")
+
+main("books/frankenstien.txt")
